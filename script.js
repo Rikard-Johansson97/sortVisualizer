a44b203cab9a9291c5b8580d15b5ea33b73d96d0
Rikard Johansson <Rikard.com@hotmail.com>
2022-12-06 09:46:10 +0100

    Quick sort delay

--- script.js
+++ script.js
@@ -75,7 +75,6 @@ window.onload = function () {
     // loops trough every element i array and give them the color green.
     const displayGreen = () => {
         sortedStacks = document.querySelectorAll(".sorted");
-        console.log(sortedStacks);
         sortedStacks.forEach((item) => {
             item.style.background = "green";
         });
@@ -128,78 +127,63 @@ window.onload = function () {
 
     // QUICK SORT
 
-    function partition(arr, start, end) {
-        // Taking the last element as the pivot
-        const pivotValue = arr[end];
-        let pivotIndex = start;
-        for (let i = start; i < end; i++) {
-            if (arr[i] < pivotValue) {
-                // Swapping elements
-                [arr[i], arr[pivotIndex]] = [arr[pivotIndex], arr[i]];
-                // Moving to next element
-                pivotIndex++;
-            }
-        }
-
-        // Putting the pivot value in the middle
-        [arr[pivotIndex], arr[end]] = [arr[end], arr[pivotIndex]];
-        return pivotIndex;
-    }
-
     async function quickSortIterative(arr) {
         // Creating an array that we'll use as a stack, using the push() and pop() functions
-        stack = [];
+        STACKS = [];
 
         // Adding the entire initial array as an "unsorted subarray"
-        stack.push(0);
-        stack.push(arr.length - 1);
+        STACKS.push(0);
+        STACKS.push(arr.length - 1);
 
         // There isn't an explicit peek() function
         // The loop repeats as long as we have unsorted subarrays
-        while (stack[stack.length - 1] >= 0) {
+        while (STACKS[STACKS.length - 1] >= 0) {
             // Extracting the top unsorted subarray
-            end = stack.pop();
-            start = stack.pop();
-            pivotIndex = partition(arr, start, end);
-
+            end = STACKS.pop();
+            start = STACKS.pop();
+            pivotIndex = await partition(arr, start, end);
             // If there are unsorted elements to the "left" of the pivot,
-            // we add that subarray to the stack so we can sort it later
+            // we add that subarray to the STACKS so we can sort it later
             if (pivotIndex - 1 > start) {
-                stack.push(start);
-                stack.push(pivotIndex - 1);
+                STACKS.push(start);
+                STACKS.push(pivotIndex - 1);
             }
 
             // If there are unsorted elements to the "right" of the pivot,
-            // we add that subarray to the stack so we can sort it later
+            // we add that subarray to the STACKS so we can sort it later
             if (pivotIndex + 1 < end) {
-                stack.push(pivotIndex + 1);
-                stack.push(end);
+                STACKS.push(pivotIndex + 1);
+                STACKS.push(end);
             }
         }
     }
 
-    function partition(arr, start, end) {
+    async function partition(arr, start, end) {
         // Taking the last element as the pivot
+        console.log("aaa");
         const pivotValue = arr[end];
         let pivotIndex = start;
         for (let i = start; i < end; i++) {
             if (arr[i] < pivotValue) {
+                await Sleep(delay);
                 // Swapping elements
                 [arr[i], arr[pivotIndex]] = [arr[pivotIndex], arr[i]];
                 // Moving to next element
                 pivotIndex++;
+                displayNumbers(arraySize, pivotIndex, arr[i]);
             }
         }
 
         // Putting the pivot value in the middle
         [arr[pivotIndex], arr[end]] = [arr[end], arr[pivotIndex]];
+        console.log(pivotIndex);
         return pivotIndex;
     }
 
-    quick.addEventListener("click", () => {
-        quickSortIterative(arraySize);
-        displayGreen();
+    quick.addEventListener("click", async () => {
+        await quickSortIterative(arraySize);
         displayNumbers(arraySize);
+        displayGreen();
     });
 
     //! ON START
